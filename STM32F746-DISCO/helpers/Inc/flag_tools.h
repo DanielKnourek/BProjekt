59a32bfb0a883c87a1be998189b92fb8d0928c29
DanielKnourek <dev.danielknourek@gmail.com>
2023-04-04 19:13:00 +0200

    feat: header file init - flag_tools.h

---
+++ STM32F746-DISCO/helpers/Inc/flag_tools.h
@@ -0,0 +1,16 @@
+/*
+ * flag_tools.h
+ *
+ *  Created on: Apr 4, 2023
+ *      Author: Daniel Kňourek
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef INC_FLAG_TOOLS_H_
+#define INC_FLAG_TOOLS_H_
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+#endif /* INC_FLAG_TOOLS_H_ */
