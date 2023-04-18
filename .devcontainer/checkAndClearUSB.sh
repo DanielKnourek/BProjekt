3b81bad5746ef886eb6ba090bb7a378d867e9eb3
Daniel Kňourek <knourek.daniel@gmail.com>
2023-04-18 05:35:55 +0000

    feat: usbip automount

--- .devcontainer/checkAndClearUSB.sh
+++ .devcontainer/checkAndClearUSB.sh
@@ -10,5 +10,19 @@ elif [ -c '/dev/ttyACM0' ]; then
     echo "USB device found as character"
     echo "everything is fine"
 else
-    echo "USB device not found"
+    echo "USB device not found... trying to mount predefinded port"
+    /mnt/c/Program\ Files/usbipd-win/usbipd.exe wsl attach --busid 1-3 -d Arch
+
+    ls -la /dev/ | grep ACM
+    if [ -d '/dev/ttyACM0' ]; then
+        echo "USB device found as direcotry"
+        sudo rm -rf /dev/ttyACM0
+        echo "USB device removed"
+
+    elif [ -c '/dev/ttyACM0' ]; then
+        echo "USB device found as character"
+        echo "everything is fine"
+    else
+        echo "USB device not found... trying to mount predefinded port"
+    fi
 fi
