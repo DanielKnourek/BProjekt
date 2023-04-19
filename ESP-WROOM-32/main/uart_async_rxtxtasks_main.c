5ea9b1f8a0808cbb55b4d18086fc94207d21d7b6
Daniel Kňourek <knourek.daniel@gmail.com>
2023-04-19 21:43:23 +0000

    feat: LD1 on BTN0 hold

--- ESP-WROOM-32/main/uart_async_rxtxtasks_main.c
+++ ESP-WROOM-32/main/uart_async_rxtxtasks_main.c
@@ -18,7 +18,10 @@ static const int RX_BUF_SIZE = 1024;
 
 #define TXD_PIN (17)
 #define RXD_PIN (16)
+#define LD_BLUE (2)
+#define BTN0 (0)
 
+uint8_t ld_enabled = 1;
 void init(void)
 {
     const uart_config_t uart_config = {
@@ -33,6 +36,10 @@ void init(void)
     uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
     uart_param_config(UART_NUM_1, &uart_config);
     uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    gpio_pad_select_gpio(LD_BLUE);
+    gpio_set_direction(LD_BLUE, GPIO_MODE_OUTPUT);
+    gpio_pad_select_gpio(BTN0);
+    gpio_set_direction(BTN0, GPIO_MODE_INPUT);
 }
 
 int sendData(const char *logName, const char *data)
@@ -51,6 +58,17 @@ static void tx_task(void *arg)
     {
         sendData(TX_TASK_TAG, "Hello ESP");
         vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+        // if (ld_enabled > 0)
+        // {
+        //     ld_enabled = 0;
+        //     gpio_set_level(LD_BLUE, 1);
+        // }
+        // else
+        // {
+        //     ld_enabled = 0;
+        //     gpio_set_level(LD_BLUE, 0);
+        // }
     }
 }
 
@@ -72,9 +90,30 @@ static void rx_task(void *arg)
     free(data);
 }
 
+static void btn_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "BTN_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+
+    while (true)
+    {
+        /* code */
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        if (gpio_get_level(BTN0) > 0)
+        {
+            gpio_set_level(LD_BLUE, 0);
+        }
+        else
+        {
+            gpio_set_level(LD_BLUE, 1);
+        }
+    }
+}
+
 void app_main(void)
 {
     init();
     xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
     xTaskCreate(tx_task, "uart_tx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
+    xTaskCreate(btn_task, "uart_tx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 2, NULL);
 }
