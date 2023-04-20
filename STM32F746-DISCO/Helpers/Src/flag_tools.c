f54ddd5d78c8f13236d830aab405e132b771152c
DanielKnourek <dev.danielknourek@gmail.com>
2023-04-20 10:15:15 +0200

    fix: flagtools lib tmp fix

---
+++ STM32F746-DISCO/Helpers/Src/flag_tools.c
@@ -0,0 +1,38 @@
+/////*
+//// * flag_tools.c
+//// *
+//// *  Created on: Apr 4, 2023
+//// *      Author: Daniel Kňourek
+//// */
+////
+/////* Includes ------------------------------------------------------------------*/
+//#include "flag_tools.h"
+////
+/////* Exported types ------------------------------------------------------------*/
+////
+/////* Exported constants --------------------------------------------------------*/
+////
+/////* Exported functions --------------------------------------------------------*/
+//////flag_set init_flags(){
+//////    flag_set flags;
+//////    flags.set = (uint8_t)1 << 4;
+//////    return flags;
+//////}
+////int get_one(){
+////	return 1;
+////}
+//void init_flags(flag_set *flags){
+//	flags->set = 0;
+//}
+//
+//void set_flag(flag_set *flags, uint8_t flag_mask){
+//    flags->set = flags->set | flag_mask;
+//}
+//
+//void reset_flag(flag_set *flags, uint8_t flag_mask){
+//    flags->set = flags->set & ~flag_mask;
+//}
+//
+//uint8_t is_set(flag_set *flags, uint8_t flag_mask){
+//    return (flags->set & flag_mask) == flag_mask;
+//}
