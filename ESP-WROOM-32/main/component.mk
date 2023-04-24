12abc637d6291a27420bdbd06e1b30cb20da2280
Daniel Kňourek <knourek.daniel@gmail.com>
2023-04-24 14:12:40 +0000

    feat: demo file server example

--- ESP-WROOM-32/main/component.mk
+++ ESP-WROOM-32/main/component.mk
@@ -1,3 +1,7 @@
 #
-# Main Makefile. This is basically the same as a component makefile.
+# "main" pseudo-component makefile.
 #
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_EMBED_FILES := favicon.ico
+COMPONENT_EMBED_FILES += upload_script.html
