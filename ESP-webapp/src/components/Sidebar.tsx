b5871a916162ba17df282d30a99834bd8c05a382
Daniel Kňourek <knourek.daniel@gmail.com>
2023-04-26 17:36:03 +0000

    feat: added tailwind, test of components

---
+++ ESP-webapp/src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const sideBar: React.FC = () => {
+
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-sky-200">
+            This is a side bar.
+        </div>
+    );
+}
+
+export default sideBar;
