b8ea0a63a3e2de0e81a0ba6fb19673587168dce9
Daniel Kňourek <knourek.daniel@gmail.com>
2023-04-30 03:50:11 +0000

    feat: webapp gzipped build

--- ESP-webapp/vite.config.ts
+++ ESP-webapp/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  let plugins: PluginOption[] | ReturnType<typeof viteCompression> = [];
+
+  plugins.push(react());
+
+  if (mode == "gz") {
+    plugins.push(viteCompression());
+
+    return {
+      plugins: plugins,
+      build: {
+        outDir: "dist_gz",
+      },
+    };
+  }
+  return { plugins };
 });
