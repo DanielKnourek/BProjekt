1c920241518a12cddf6c94ef653ee1391b692385
Daniel Kňourek <knourek.daniel@gmail.com>
2026-03-28 10:01:21 +0000

    feat: demo reciving data frame

--- ESP-WROOM-32/main/lib/serial_link/src/serial_link.c
+++ ESP-WROOM-32/main/lib/serial_link/src/serial_link.c
@@ -4,8 +4,9 @@
 // #include "driver/gpio.h"
 // #include "string.h"
 #include "esp_log.h"
+#include <string.h>
 
-static const char *TAG = "serial_link.c";
+static const char* TAG = "serial_link.c";
 
 static const int RX_BUF_SIZE = 128;
 
@@ -52,59 +53,115 @@ void init(void) {
 //         vTaskDelay(2000 / portTICK_PERIOD_MS);
 //     }
 // }
-void deserialize(uint8_t *data) {
-
+void deserialize(uint8_t* data, size_t len) {
     // deserialize pb
-    //  -- read MessageID FIRST to know the type of message, then read the rest of the data accordingly --
+    //  -- read MessageID FIRST to know the type of message, then read the rest
+    //  of the data accordingly --
     
-    MessageID *msg_id;
     unsigned len_id1 = message_id__get_packed_size(&(MessageID)MESSAGE_ID__INIT);
-
-    msg_id = message_id__unpack(NULL, len_id1, data);
+    MessageID* msg_id = message_id__unpack(NULL, len_id1, data);
     if (msg_id == NULL) {
         ESP_LOGE(TAG, "error unpacking incoming message_id__unpack");
         return;
     }
 
+    // size_t len_id1 = message_id__get_packed_size(msg_id);
+
     // display the message's fields.
     ESP_LOGI(TAG, "deserialize: id=%d" PRIi32,
              msg_id->id);  // required field
 
+    if (msg_id->id == MESSAGE_TYPE__LINK1_DATA) {
+        if (len <= len_id1) {
+            ESP_LOGE(TAG, "Not enough data for Link1Data");
+            message_id__free_unpacked(msg_id, NULL);
+            return;
+        }
+
+            uint8_t aligned_log_buffer[128];
+
+            // 2. Calculate the length of the data we want to log.
+            size_t log_len = len - len_id1;
+
+            // // 3. To be safe, make sure we don't copy more data than our temp buffer can hold.
+            if (log_len > sizeof(aligned_log_buffer)) {
+                ESP_LOGW(TAG, "Log data is too large for temp buffer, truncating from %d to %d bytes.", log_len, sizeof(aligned_log_buffer));
+                // log_len = sizeof(aligned_log_buffer);
+            }
+
+
+            // 4. Copy the (potentially unaligned) data slice into our (definitely aligned) temp buffer.
+            memcpy(aligned_log_buffer, data + len_id1, log_len);
+            
+
+            // 5. Now, call the hexdump function with the aligned buffer. This should work.
+            ESP_LOGI(TAG, "Dumping buffer from aligned temporary copy:");
+            ESP_LOG_BUFFER_HEXDUMP(TAG, aligned_log_buffer, log_len, ESP_LOG_INFO);
+
+
+        Link1Data* msg_data1 =
+            link1_data__unpack(NULL, len - len_id1, data + len_id1);
+        if (msg_data1 == NULL) {
+            ESP_LOGE(TAG, "error unpacking incoming link1_data__unpack");
+            message_id__free_unpacked(msg_id, NULL);
+            return;
+        }
+
+        // display the message's fields.
+        ESP_LOGI(TAG, "deserialize data: id=%d; data:%d" PRIi32, msg_data1->id,
+                 (int)msg_data1->sensor_data);
+
+        // Free the allocated deserialized buffer
+        link1_data__free_unpacked(msg_data1, NULL);
+    }
+
     // Free the allocated deserialized buffer
     message_id__free_unpacked(msg_id, NULL);
-
-    // -- read data1
-
-    Link1Data *msg_data1;
-    unsigned len_data1 = link1_data__get_packed_size(&(MessageID)LINK1_DATA__INIT);
-
-    if (sizeof(data) > len_id1 + len_data1) {
-        ESP_LOGI(TAG, "Not enough data received to unpack Link1Data, expected at least %d bytes but got %d bytes", len_id1 + len_data1, sizeof(data));
-        return;
-    }
-    msg_data1 = link1_data__unpack(NULL, len_data1, data + len_id1);
-    if (msg_data1 == NULL) {
-        ESP_LOGE(TAG, "error unpacking incoming link1_data__unpack");
-        return;
-    }
-
-    // display the message's fields.
-    ESP_LOGI(TAG, "deserialize data: id=%d; data:%d" PRIi32, msg_data1->id,
-             (int)msg_data1->sensor_data);
 }
-static void rx_task(void *arg) {
-    static const char *RX_TASK_TAG = "RX_TASK";
+static void rx_task(void* arg) {
+    static const char* RX_TASK_TAG = "RX_TASK";
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
-    uint8_t *data = (uint8_t *)malloc(RX_BUF_SIZE + 1);
+    uint8_t* data = (uint8_t*)malloc(RX_BUF_SIZE + 1);
     while (1) {
         const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE,
                                             1000 / portTICK_PERIOD_MS);
         if (rxBytes > 0) {
             data[rxBytes] = 0;
-            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, (char *)data);
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s' |1ca5c|", rxBytes, (char*)data);
             ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
 
-            deserialize(data);
+            // TODO: TEMP struct to test serialization bytes
+            Link1Data msgdata = LINK1_DATA__INIT;
+            msgdata.id = MESSAGE_TYPE__LINK1_DATA;
+            msgdata.sensor_data = 69;
+
+            unsigned len = link1_data__get_packed_size(&msgdata);
+            uint8_t* buf = (uint8_t*)malloc(32);
+            if (buf != NULL) {
+                memset(buf, 0, 32);
+            }
+
+            // test BEGIN
+            link1_data__pack(&msgdata, buf);
+            ESP_LOGI(RX_TASK_TAG, "Serialized MessageID of length %d bytes", len);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, buf, 32, ESP_LOG_INFO);
+
+            Link1Data* msg_data2;
+            msg_data2 = link1_data__unpack(NULL, 4, buf);
+            if (msg_data2 == NULL) {
+                ESP_LOGE(RX_TASK_TAG, "error unpacking incoming link1_data__unpack");
+                
+            } else {
+
+                // ESP_LOGI(RX_TASK_TAG, "Serialized msg_data2 of length %d bytes", 32);
+                ESP_LOGI(RX_TASK_TAG, "Deserialized msg_data2: id=%d; data:%d" PRIi32, msg_data2->id,
+                    (int)msg_data2->sensor_data);
+            }
+                    // test END
+            
+            // deserialize(data, rxBytes);
+            
+            free(buf);
         }
     }
     free(data);
@@ -112,8 +169,11 @@ static void rx_task(void *arg) {
 
 void uart_init(void) {
     init();
-    xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL,
+    // TODO: document, there was not enough memory for the task
+    xTaskCreate(rx_task, "uart_rx_task", 1024 * 4, NULL,
                 configMAX_PRIORITIES - 1, NULL);
+    // xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL,
+    //             configMAX_PRIORITIES - 1, NULL);
     // xTaskCreate(tx_task, "uart_tx_task", 1024 * 2, NULL,
     //             configMAX_PRIORITIES - 2, NULL);
 }
