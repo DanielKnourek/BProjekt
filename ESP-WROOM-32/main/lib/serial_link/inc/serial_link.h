64e902d3b43227803f7bec548f24c58a3a0f2536
Daniel Kňourek <knourek.daniel@gmail.com>
2026-03-29 10:06:07 +0000

    feat: auto free attr

--- ESP-WROOM-32/main/lib/serial_link/inc/serial_link.h
+++ ESP-WROOM-32/main/lib/serial_link/inc/serial_link.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "messenger.pb-c.h"
+#include "protobuf-c/protobuf-c.h"
 
 void test(void);
--- ESP-WROOM-32/main/lib/serial_link/src/serial_link.c
+++ ESP-WROOM-32/main/lib/serial_link/src/serial_link.c
@@ -13,6 +13,25 @@ static const int RX_BUF_SIZE = 128;
 #define TXD_PIN (CONFIG_UART_GPIO_TX)
 #define RXD_PIN (CONFIG_UART_GPIO_RX)
 
+#define __AUTO_FREE_MSG__ __attribute__((cleanup(auto_free_message)))
+
+/*
+* @note Cleanup function for ProtobufCMessage pointers.
+*       It will automatically free the memory allocated for a ProtobufCMessage when the variable goes out of scope.
+*/
+static inline void auto_free_message(void *ptr) {
+    // Cast the generic void* back to a pointer-to-a-ProtobufCMessage-pointer
+    ProtobufCMessage **msg_ptr = (ProtobufCMessage **)ptr;
+    
+    if (msg_ptr != NULL && *msg_ptr != NULL) {
+        ESP_LOGD("CLEANUP", "Automatically freeing message of type %s.", (*msg_ptr)->descriptor->name);
+        // Use the universal protobuf-c free function!
+        protobuf_c_message_free_unpacked(*msg_ptr, NULL);
+        *msg_ptr = NULL; // Nullify to prevent dangling pointers
+    }
+}
+
+
 void init(void) {
     const uart_config_t uart_config = {
         .baud_rate = 115200,
@@ -53,28 +72,32 @@ void init(void) {
 //         vTaskDelay(2000 / portTICK_PERIOD_MS);
 //     }
 // }
-void deserialize(uint8_t* data, size_t len_incoming) {
-    // deserialize pb
+
+/* @note Caller is responsible for calling frame_payload__free_unpacked() on the returned pointer.  
+*
+*   use __AUTO_FREE_MSG__ to automatically free the message when it goes out of scope, or call frame_payload__free_unpacked() manually when done with the message.
+*/
+FramePayload* create_frame_payload(uint8_t* data, size_t rxBytes) {
+    // create_frame_payload pb
     //  -- read FrameHeader FIRST to know the type of message, then read the rest
     //  of the data accordingly
     
     // TODO: len_header could be made static const to avoid recomputing the size every time, but this is just a test for now
     unsigned len_header = frame_header__get_packed_size(&(FrameHeader)FRAME_HEADER__INIT);
-    FrameHeader* msg_header = frame_header__unpack(NULL, len_header, data);
+    __AUTO_FREE_MSG__ FrameHeader* msg_header = frame_header__unpack(NULL, len_header, data);
     if (msg_header == NULL) {
         ESP_LOGE(TAG, "error unpacking incoming message_id__unpack");
-        return;
+        return NULL;
     }
 
     // display the message's fields. TODO: remove after testing
-    ESP_LOGI(TAG, "deserialize header: id=%d; data:%d" PRIi32, 
+    ESP_LOGI(TAG, "create_frame_payload header: id=%d; data:%d" PRIi32, 
             (int)msg_header->next_message_size,
              (int)msg_header->crc);
 
-    if (len_header + msg_header->next_message_size > len_incoming) {
+    if (len_header + msg_header->next_message_size > rxBytes) {
         ESP_LOGE(TAG, "Not enough data for FramePayload");
-        frame_header__free_unpacked(msg_header, NULL);
-        return;
+        return NULL;
     }
     
     FramePayload* msg_payload = frame_payload__unpack(NULL, 
@@ -83,34 +106,36 @@ void deserialize(uint8_t* data, size_t len_incoming) {
 
     if (msg_payload == NULL) {
         ESP_LOGE(TAG, "error unpacking incoming link1_data__unpack");
-        frame_header__free_unpacked(msg_header, NULL);
-        return;
+        return NULL;
     }
 
     // display the message's fields. TODO: remove after testing
-    ESP_LOGI(TAG, "deserialize data: data:%d" PRIi32,
+    ESP_LOGI(TAG, "create_frame_payload data: data:%d" PRIi32,
              (int)msg_payload->test1_data->test_data);
 
-    // Free the allocated deserialized buf_header
-    frame_header__free_unpacked(msg_header, NULL);
-    frame_payload__free_unpacked(msg_payload, NULL);
-
+    return msg_payload;
 }
 
 static void rx_task(void* arg) {
     static const char* RX_TASK_TAG = "RX_TASK";
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    ESP_LOGI(RX_TASK_TAG, "---- Recieving new data ----");
+
     uint8_t* data = (uint8_t*)malloc(RX_BUF_SIZE + 1);
     while (1) {
         const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE,
                                             1000 / portTICK_PERIOD_MS);
         if (rxBytes > 0) {
             data[rxBytes] = 0;
+
+            // TODO: remove after testing
             ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s' |1ca5c|", rxBytes, (char*)data);
             ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
 
-            deserialize(data, rxBytes);
-            
+            __AUTO_FREE_MSG__ FramePayload* recieved_data = create_frame_payload(data, rxBytes);
+            ESP_LOGI(RX_TASK_TAG, "Received data: %d" PRIi32, (int)recieved_data->test1_data->test_data);
+            // frame_payload__free_unpacked(recieved_data, NULL);
+
         }
     }
     free(data);
